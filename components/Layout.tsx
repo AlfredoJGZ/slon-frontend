b149f8ea7158ace549f965fced2a3ce138c8043f
rinconeliezer <rinconeliezer@gmail.com>
2022-10-20 12:34:19 -0400

    Footer Created

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,4 +1,5 @@
 import Navbar from "./Navbar";
+import Footer from "./Footer";
 
 type LayoutType = {
   children: React.ReactElement;
@@ -9,7 +10,7 @@ const Layout = ({ children }: LayoutType) => {
     <>
       <Navbar />
       {children}
-      <Navbar />
+      <Footer />
     </>
   );
 };
